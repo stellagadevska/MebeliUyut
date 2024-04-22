201c830d6205179cd26882fc6f07fb6cfac74a09
Stella Gadevska <58764584+stellagadevska@users.noreply.github.com>
2024-04-22 15:42:29 +0300

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import { motion } from "framer-motion";
+
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/Home.css";
+
+import heroImg from "../assets/images/hero-img.png";
+
 const Home = () => {
-    return <div>Home</div>
+    const year = new Date().getFullYear();
+    return (
+        <Helmet title='Home'>
+            <section className='hero_section'>
+                <Container>
+                    <Row>
+                        <Col lg='6' md='6'>
+                            <div className='hero_content'>
+                                <p className='hero_subtitle'>Trending product in {year}</p>
+                                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum
+                                    dolore eu fugiat nulla pariatur.
+                                </p>
+
+                                <motion.button whileTap={{ scale: 1.2 }} className='buy_btn'>
+                                    <Link to='/shop'>SHOP NOW</Link>
+                                </motion.button>
+                            </div>
+                        </Col>
+                        <Col lg='6' md='6'>
+                            <div className='hero_image'>
+                                <img src={heroImg} alt='' />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
 export default Home;
