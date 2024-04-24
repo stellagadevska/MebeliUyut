716ff0d7a6b1ab8550adac29c4c5906d87a4dae1
Stella Gadevska <58764584+stellagadevska@users.noreply.github.com>
2024-04-24 15:06:14 +0300

    Home Page Components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 import { motion } from "framer-motion";
+import products from "../assets/data/products";
 
 import Helmet from "../components/Helmet/Helmet";
+import Services from "../services/Services";
+import ProductsList from "../components/UI/ProductsList";
 import "../styles/Home.css";
 
 import heroImg from "../assets/images/hero-img.png";
 
 const Home = () => {
+    const [trendingProducts, setTrendingProducts] = useState([]);
+    const [bestSalesProducts, setBestSalesProducts] = useState([]);
+
+    useEffect(() => {
+        const filteredTrendingProducts = products.filter(
+            (item) => item.category === "chair"
+        );
+
+        const filteredBestSalesProducts = products.filter(
+            (item) => item.category === "sofa"
+        );
+
+        setTrendingProducts(filteredTrendingProducts);
+        setBestSalesProducts(filteredBestSalesProducts);
+    }, []);
+
     const year = new Date().getFullYear();
     return (
         <Helmet title='Home'>
@@ -39,6 +58,28 @@ const Home = () => {
                                 <img src={heroImg} alt='' />
                             </div>
                         </Col>
+                    </Row>
+                </Container>
+            </section>
+            <Services />
+            <section className='trending_products'>
+                <Container>
+                    <Row>
+                        <Col lg='12' className='text-center'>
+                            <h2 className='section_title'>Trending Products</h2>
+                        </Col>
+                        <ProductsList data={trendingProducts} />
+                    </Row>
+                </Container>
+            </section>
+
+            <section className='best_sales'>
+                <Container>
+                    <Row>
+                        <Col lg='12' className='text-center'>
+                            <h2 className='section_title'>Best Sales</h2>
+                        </Col>
+                        <ProductsList data={bestSalesProducts} />
                     </Row>
                 </Container>
             </section>
